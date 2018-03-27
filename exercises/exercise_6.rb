0252cff2b9cd2626c0d4d15c1025cd7371696628
RenatoAmaral <srkimbo@gmail.com>
2018-03-27 19:00:22 +0000

    Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,30 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 80)
+@store1.employees.create(first_name: "Steven", last_name: "Son", hourly_rate: 60)
+@store1.employees.create(first_name: "Juan", last_name: "Borba", hourly_rate: 80)
+@store1.employees.create(first_name: "Carlos", last_name: "Geronimo", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Bond", hourly_rate: 60)
+@store1.employees.create(first_name: "Will", last_name: "Smith", hourly_rate: 80)
+@store1.employees.create(first_name: "Will", last_name: "Nowhere", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Kid", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Jack", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Robert", last_name: "Doode", hourly_rate: 80)
+@store2.employees.create(first_name: "Maria", last_name: "Sony", hourly_rate: 60)
+@store2.employees.create(first_name: "Joana", last_name: "Barba", hourly_rate: 80)
+@store2.employees.create(first_name: "Bernad", last_name: "Gerd", hourly_rate: 60)
+@store2.employees.create(first_name: "Carol", last_name: "Bond", hourly_rate: 60)
+@store2.employees.create(first_name: "Ashley", last_name: "Smith", hourly_rate: 80)
+@store2.employees.create(first_name: "Will", last_name: "Notagain", hourly_rate: 60)
