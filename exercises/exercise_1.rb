4eb3b0c62a85a37fb790267ef8e6805c801e838e
RenatoAmaral <srkimbo@gmail.com>
2018-03-27 17:42:39 +0000

    3 Stores were created, 1 was deleted

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,3 +5,14 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+
+Store.create({annual_revenue: 300000, mens_apparel: true, womens_apparel: true, name: 'Burnaby'})
+Store.create({annual_revenue: 126000, mens_apparel: false, womens_apparel: true, name: 'Richmond'})
+Store.create({annual_revenue: 190000, mens_apparel: true, womens_apparel: false, name: 'Gastown'})
+
+
+ puts "******   There are #{Store.count} stores in the Database   ******\n\n"
